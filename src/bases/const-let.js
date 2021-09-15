0eb903fda00980aabfe339b8a7d128ae47df7452
Edgar <edgar.lazaro@tismart.com>
2021-09-15 11:26:44 -0500

    1er commit

---
+++ src/bases/const-let.js
@@ -0,0 +1,19 @@
+//Variables y constantes
+
+const nombre = "Edgar";
+const apelliddo = "Lazaro";
+
+var edad = 0;
+let edad2 = 0;
+
+const anioNacimiento = 1988;
+const anioActual = 2021;
+if (anioActual === 2021) {
+  let edad = 0;
+  edad = anioActual - anioNacimiento;
+  console.log("Edad actual:", edad);
+} else {
+}
+edad = 10;
+console.log("Edad actual entorno global:", edad);
+// console.log("Edad 2 actual:", edad2);
